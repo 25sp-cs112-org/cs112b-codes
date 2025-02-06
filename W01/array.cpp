396d3261e031a2868e5b878f29ee21c343c90c64
Eric Araujo <eric.araujo@calvin.edu>
2025-02-06 15:09:08 -0500

    Changes to be committed: 	new file:   W01/arr 	modified:   W01/array.cpp

--- W01/array.cpp
+++ W01/array.cpp
@@ -6,20 +6,20 @@ Description: Lecture about arrays and tests done in class on 01/31.
 #include <iostream>
 using namespace std;
 
-
 int triple(int x = 1);
 
 int main() {
-    int arr[5] = {1, 2, 3, 4};
-    // cout << arr[6] << endl;
-    cout << arr << endl;
-    cout << arr[1] << endl;
-    cout << &arr[1] << endl;
-    cout << sizeof(arr) << endl;
-    cout << sizeof(arr[1]) << endl;
-    cout << sizeof(&arr[1]) << endl;
+    int arr[5] = {1, 2, 3, 4, 5}; // Fixed array initialization
+    // cout << arr[6] << endl; // Commented out to avoid out-of-bounds access
+    cout << "Array address: " << arr << endl;
+    cout << "Second element: " << arr[1] << endl;
+    cout << "Address of second element: " << &arr[1] << endl;
+    cout << "Size of array: " << sizeof(arr) << endl;
+    cout << "Size of array element: " << sizeof(arr[1]) << endl;
+    cout << "Size of address of array element: " << sizeof(&arr[1]) << endl;
 
-    cout << triple() << endl;
+    cout << "Triple of default value: " << triple() << endl;
+    cout << "Triple of 5: " << triple(5) << endl; // Added another test for triple function
 }
 
 int triple(int x){
